00b373e594452b2952df1307007204ac9497702e
Diego <diegolmi@hotmail.com>
2021-03-10 11:40:04 -0300

    Revert "deploy firebase"
    This reverts commit 301eca0b48b69057725726cde09ea7495740148b.

--- components/ui/Buscar.js
+++ components/ui/Buscar.js
@@ -1,8 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import React, {useState} from 'react';
+import React from 'react';
 import styled from "@emotion/styled";
-import Router from 'next/router';
 
 const InputText = styled.input`
     border: 1px solid gray;
@@ -29,23 +28,13 @@ text-indent: -9999px;
 }
 `
 const Buscar = () => {
-    const [busqueda, setBusqueda] = useState('');
-    const buscarProducto = e => {
-        e.preventDefault();
-       if(busqueda.trim() === '') return;
-       Router.push({
-           pathname: '/buscar',
-           query: {'q' : busqueda}
-       })
-    }
     return (
         <form 
         css={css`
         position: relative;
         `}
-        onSubmit={buscarProducto}
         >
-            <InputText type="text" placeholder="Buscar Mascotas" onChange={e => setBusqueda(e.target.value)} />
+            <InputText type="text" placeholder="Buscar Mascotas" />
             <InputSubmit type="submit">Buscar</InputSubmit>
         </form>
     )
--- pages/buscar.js
+++ pages/buscar.js
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import Layout from '../components/layout/Layout';
-import { useRouter } from 'next/router';
-import DetallesMascotas from '../components/layout/DetallesMascotas';
-import Home from '../pages/index';
-const Buscar = () => {
+import Layout from '../components/layout/Layout'
 
-  const router = useRouter();
-  const { query: { q }} = router;
-
-  const { mascotas } = Home()
-  const [ resultado, guardarResultado ] = useState([]);
-
-  console.log(mascotas)
-
-  useEffect(() => {
-    const busqueda = q.toLoweCase();
-    const filtro =  mascotas.filter(mascota => {
-      return (
-        mascota.nombre.toLowerCase().includes(busqueda) || 
-        mascota.descripcion.toLowerCase().includes(busqueda)
-      )
-    });
-    guardarResultado(filtro);
-    
-}, [ q, mascotas ]);
-
-return (
+ const Buscar = () => {
+  return (
     <div>
       <Layout>
 
